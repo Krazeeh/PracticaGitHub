e550350a4d7ceaf23791674562270e869f7b89d7
Olga <Olga@192.168.1.41>
2017-03-10 00:08:41 +0100

    Prueba Git

---
+++ EjercicioGitub/src/ejerciciogitub/EjercicioGitub.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejerciciogitub;
+
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author Olga
+ */
+public class EjercicioGitub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        int num;
+        char cont, cont2;
+        
+        Scanner lector = new Scanner (System.in);
+        
+        do{
+            System.out.println("Introduzca un numero para mostrar la piramide correspondiente.");
+            num = lector.nextInt();
+        }while(num<1);
+        
+        for (cont='A';cont<=(char)('A'+num);cont++){
+            for (cont2='A';cont2<cont;cont2++){
+                System.out.print(cont2);
+            }
+            System.out.println();
+        }
+        for (cont=(char)('A'+num);cont>'A';cont--){
+            for (cont2='A';cont2<cont;cont2++){
+                System.out.print(cont2);
+            }
+            System.out.println();
+        }
+    }
+    
+}
